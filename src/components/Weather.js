481ce03f692635739a1afc6b92ef796aea30c6a2
Chris Czach <chrisczach@gmail.com>
2019-03-15 10:23:55 -0700

    add weather fetch and add api keys to .env

---
+++ src/components/Weather.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+export default ({ latitude, longitude }) => {
+  const [forecast, setForecast] = useState()
+
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${
+      process.env.REACT_APP_OWM_API_KEY
+    }`
+  )
+    .then(response => response.json())
+    .then(result => setForecast(result))
+  return forecast ? (
+    <div>Weather Forecast: {JSON.stringify(forecast)}</div>
+  ) : (
+    <div>Forecast Loading</div>
+  )
+}
