ea149fd15fc9063cd5346c3f5e92036aaee32f9a
Christopher Czach <ChrisCzach@ChristophersMBP.socal.rr.com>
2019-03-14 22:41:08 -0700

    add geolocation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { geolocated } from 'react-geolocated'
 
-class App extends Component {
-  render() {
-    return <div>Hello, V8 Geckos Team 12!!!</div>
-  }
+const App = ({
+  coords,
+  isGeolocationAvailable, // boolean flag indicating that the browser supports the Geolocation API
+  isGeolocationEnabled, // boolean flag indicating that the user has allowed the use of the Geolocation API
+  positionError, // object with the error returned from the Geolocation API call
+}) => {
+  try {
+    var {
+      latitude,
+      longitude,
+      altitude,
+      accuracy,
+      altitudeAccuracy,
+      heading,
+      speed,
+    } = coords
+  } catch (err) {}
+  return !isGeolocationAvailable ? (
+    <div>Your browser does not support Geolocation</div>
+  ) : !isGeolocationEnabled ? (
+    <div>Geolocation is not enabled</div>
+  ) : coords ? (
+    <ul>
+      <li>Latitude: {latitude}</li>
+      <li>Longitude: {longitude}</li>
+      <li>Accuracy: {accuracy}</li>
+      <li>Altitude: {altitude}</li>
+      <li>Altitude Accuracy: {altitudeAccuracy}</li>
+      <li>Heading: {heading}</li>
+      <li>Speed: {speed}</li>
+    </ul>
+  ) : (
+    <div>Getting the location data&hellip; </div>
+  )
 }
 
-export default App
+export default geolocated({
+  positionOptions: {
+    enableHighAccuracy: false,
+  },
+  userDecisionTimeout: 5000,
+})(App)
