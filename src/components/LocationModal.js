3d3ac916eeb9e20c8fe72241aaba37f904ee2c23
Radha Banerjee <meikopolis@gmail.com>
2019-03-17 23:25:00 -0700

    Merge branches news-section and development (#12)
    * add styles
    
    * .

--- src/components/LocationModal.js
+++ src/components/LocationModal.js
@@ -5,8 +5,12 @@ import styled from 'styled-components'
 export default ({ setLocation }) => {
   return (
     <Modal>
+      <Header>
+        <h1>where you at?</h1>
+        <h2>location was unavailable from browser</h2>
+      </Header>
       <StyledAngoliaBox
-        placeholder={`Couldn't get location from browser. Please enter address.`}
+        placeholder={`Please enter location`}
         options={{
           appId: process.env.REACT_APP_ANGOLIA_APP_ID,
           apiKey: process.env.REACT_APP_ANGOLIO_API_KEY,
@@ -19,40 +23,66 @@ export default ({ setLocation }) => {
           },
           suggestionIndex,
         }) => setLocation({ latitude: lat, longitude: lng, available: true })}
-        // onSuggestions={({ rawAnswer, query, suggestions }) =>
-        //   console.log(
-        //     'Fired when dropdown receives suggestions. You will receive the array of suggestions that are displayed.'
-        //   )
-        // }
-        // onCursorChanged={({ rawAnswer, query, suggestion, suggestonIndex }) =>
-        //   console.log('Fired when arrows keys are used to navigate suggestions.')
-        // }
-        // onClear={() => console.log('Fired when the input is cleared.')}
-        // onLimit={({ message }) =>
-        //   console.log('Fired when you reached your current rate limit.')
-        // }
-        // onError={({ message }) =>
-        //   console.log(
-        //     'Fired when we could not make the request to Algolia Places servers for any reason but reaching your rate limit.'
-        //   )
-        // }
       />
     </Modal>
   )
 }
 
+const Header = styled.div`
+  position: relative;
+  color: rgba(255, 255, 255, 0.9);
+  margin: 10vmin auto;
+  & h1 {
+    font-size: 4em;
+    @media screen and (orientation: portrait) {
+      margin-top: 10vh;
+    }
+  }
+  & h2 {
+    opacity: 0.5;
+    font-size: 1.4em;
+    margin: 1em 0;
+  }
+  @media screen and (orientation: portrait) {
+    & h1 {
+      font-size: 3em;
+    }
+    & h2 {
+      opacity: 0.5;
+      font-size: 1em;
+      margin: 0.5em 0;
+    }
+  }
+`
+
 const Modal = styled.div`
+  z-index: 100;
+  display: flex;
+  flex-direction: column;
   position: fixed;
   top: 0;
-  background: navy;
-  color: white;
+  background: #0f2027;
+  background: -webkit-linear-gradient(
+    to top left,
+    #2c5364,
+    #203a43,
+    #0f2027
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to top left, #2c5364, #203a43, #0f2027);
+
+  color: #203a43;
   height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: center;
   align-items: center;
 `
 
 const StyledAngoliaBox = styled(AlgoliaPlaces)`
+  opacity: 0.9;
+  color: #203a43;
   width: 75vw;
+  margin-bottom: auto;
+  @media screen and (orientation: portrait) {
+    width: 90vw;
+  }
 `
