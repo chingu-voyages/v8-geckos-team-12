a9f2e43c64c74c1828e32d581827980adeb0ce9a
radha <meikopolis@gmail.com>
2019-03-18 22:01:38 -0700

    Add basic styling to articles

--- src/components/News.js
+++ src/components/News.js
@@ -5,6 +5,7 @@ import styled from 'styled-components'
 const newsapi = new NewsAPI(process.env.REACT_APP_NEWS_API_KEY)
 export default ({ query }) => {
   const [articles, setArticles] = useState(null)
+
   if (!articles) {
     newsapi.v2
       .topHeadlines({
@@ -15,12 +16,27 @@ export default ({ query }) => {
         setArticles(response.articles)
       })
   }
+
   return articles ? (
     <ArticleWrapper>
-      Articles:{' '}
       <ul>
-        {articles.map(article => (
-          <li>{JSON.stringify(article)}</li>
+        {articles.map((article, index) => (
+          <li key={`article-${index}`}>
+            <h5>{article.title}</h5>
+            {article.author ? (
+              <ArticleText auth>By {article.author}</ArticleText>
+            ) : null}
+            <ArticleText desc>
+              {article.content
+                ? article.content.split('[')[0]
+                : article.description}
+              <a href={article.url} target='__newtab'>
+                Read more
+              </a>
+            </ArticleText>
+
+            <ArticleText auth>{article.source.name}</ArticleText>
+          </li>
         ))}
       </ul>
     </ArticleWrapper>
@@ -30,11 +46,44 @@ export default ({ query }) => {
 }
 
 const ArticleWrapper = styled.div`
-  // add styling for div here
+  color: black;
+
   & ul {
-    //add styling for child ul here
-    & li {
-      //add stying for child li here
+    margin: auto;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    height: 65vh;
+    overflow-y: scroll;
+  }
+
+  & li {
+    padding: 10px 20px;
+    position: relative;
+    width: 70%;
+    overflow-wrap: break-word;
+    border: black solid 2px;
+    border-radius: 5px;
+    background-color: rgba(232, 234, 236, 0.8);
+    margin: 10px 5px;
+    line-height: 130%;
+
+    & h5 {
+      font-size: 19px;
+      margin-bottom: 10px;
+    }
+
+    & a {
+      color: #dd443c;
     }
   }
+
+  & li:hover {
+    transform: translateY(-10px);
+  }
+`
+
+const ArticleText = styled.p`
+  ${props => (props.auth ? 'font-style: italic' : '')};
+  ${props => (props.desc ? 'margin: 15px 0' : '')};
 `
