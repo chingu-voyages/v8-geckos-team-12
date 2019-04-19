f66545053291729ea66f723166238ed47a73ecfa
Chris Czach <chrisczach@gmail.com>
2019-04-19 08:22:28 -0700

    react integration

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import LogRocket from 'logrocket'
+import setupLogRocketReact from 'logrocket-react'
 LogRocket.init('t7irp1/geckos')
+setupLogRocketReact(LogRocket)
 
 ReactDOM.render(
   <>
