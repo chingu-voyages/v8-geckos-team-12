035e8b610f7f677ec3e02a0961238750d1ec1a52
Chris Czach <42812259+chrisczach@users.noreply.github.com>
2019-04-22 10:13:44 -0700

    only initiate logrocket on deploys and user prompt on non-master branches

--- src/index.js
+++ src/index.js
@@ -4,19 +4,26 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 import LogRocket from 'logrocket'
 import setupLogRocketReact from 'logrocket-react'
-LogRocket.init('t7irp1/geckos')
-setupLogRocketReact(LogRocket)
-const name = window.prompt(`name: `, localStorage.getItem(`name`))
-const device = window.prompt(
-  `describe device: `,
-  localStorage.getItem(`device`)
-)
-localStorage.setItem(`name`, name)
-localStorage.setItem(`device`, device)
-LogRocket.identify('THE_USER_ID_IN_YOUR_APP', {
-  name: name,
-  email: device,
-})
+console.log(process.env)
+if (process.env.NODE_ENV !== `development`) {
+  LogRocket.init('t7irp1/geckos')
+  setupLogRocketReact(LogRocket)
+  if (process.env.BRANCH !== `master`) {
+    const name = window.prompt(`name: `, localStorage.getItem(`name`))
+    const device = window.prompt(
+      `describe device: `,
+      localStorage.getItem(`device`)
+    )
+    localStorage.setItem(`name`, name)
+    localStorage.setItem(`device`, device)
+    LogRocket.identify('THE_USER_ID_IN_YOUR_APP', {
+      name: name,
+      email: device,
+    })
+  }
+} else {
+}
+
 ReactDOM.render(
   <>
     <App />
