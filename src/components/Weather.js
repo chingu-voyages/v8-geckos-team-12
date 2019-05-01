0ce2f28581a169a66817a72a54ff92fe4b4eba1d
Chris Czach <42812259+chrisczach@users.noreply.github.com>
2019-05-01 14:53:14 -0700

    fix scroll behaviour on weather 1day and 5day

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -40,11 +40,15 @@ const StyledLink = styled(Link)`
   }
 `
 const ForecastWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  overflow-y: scroll;
   //add styles here for current location info
   background: rgba(255, 255, 255, 0.85);
   grid-column: span 4;
   grid-row: span 2;
-  overflow-y: scroll;
+  overflow-y: hidden;
   box-shadow: 0 0 35px rgba(50, 50, 50, 0.4), 0 0 10px rgba(20, 20, 20, 0.4);
   border-radius: 5px;
   background-color: rgba(var(--rgb-main-light), 0.85);
